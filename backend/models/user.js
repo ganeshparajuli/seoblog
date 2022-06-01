b537abee9c685f54e84aaab504d15885700db175
ganeshparajuli <ganeshparajuli199@gmail.com>
2022-06-01 22:23:18 +0530

    TypeError: expressJwt is not a function #3

--- backend/models/user.js
+++ backend/models/user.js
@@ -53,4 +53,40 @@ const userSchema = new mongoose.Schema(
     { timestamp: true }
 );
 
+userSchema
+    .virtual('password')
+    .set(function(password) {
+        // create a temporarity variable called _password
+        this._password = password;
+        // generate salt
+        this.salt = this.makeSalt();
+        // encryptPassword
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    }
+};
+
 module.exports = mongoose.model('User', userSchema);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/auth');
 
-router.post('/signup', signup);
+const { signup, signin, signout, requireSignin } = require('../controllers/auth');
+
+
+// validators
+const { runValidation } = require('../validators');
+const { userSignupValidator, userSigninValidator } = require('../validators/auth');
+
+router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/signin', userSigninValidator, runValidation, signin);
+router.get('/signout', signout);
+// test
+router.get('/secret', requireSignin, (req, res) => {
+    res.json({
+        message: 'you have access to secret page'
+    });
+});
 
 module.exports = router;
