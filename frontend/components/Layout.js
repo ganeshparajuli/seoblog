88606ec57f092ab9ae739f399702a032e9b00a74
ganeshparajuli <ganeshparajuli199@gmail.com>
2022-06-02 15:42:48 +0530

    Frontend Signin Signup #7

---
+++ frontend/components/Layout.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+import React from 'react';
+const Layout = ({ children }) => {
+    return (
+        <React.Fragment>
+            <Header />
+            {children}
+        </React.Fragment>
+    );
+};
+
+export default Layout;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,2 +1,15 @@
-const Index= ()=> <h2>Index pages</h2>
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const Index = () => {
+    return (
+        <Layout>
+            <h2>Index page</h2>
+            <Link href="/signup">
+                <a>Signup</a>
+            </Link>
+        </Layout>
+    );
+};
+
 export default Index;
