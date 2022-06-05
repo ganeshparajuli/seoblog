89316f641545b88bec0c6bf4ab3551b6129f7919
ganeshparajuli <ganeshparajuli199@gmail.com>
2022-06-05 14:10:10 +0530

    category api bugs #11

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -87,6 +87,7 @@ exports.authMiddleware = (req, res, next) => {
 
 exports.adminMiddleware = (req, res, next) => {
     const adminUserId = req.user._id;
+    console.log("mig33",req.user)
     User.findById({ _id: adminUserId }).exec((err, user) => {
         if (err || !user) {
             return res.status(400).json({
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -49,6 +49,22 @@ const Header = () => {
               </React.Fragment>
             )}
 
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
             {isAuth() && (
               <NavItem>
                 <NavLink style={{ cursor: 'pointer' }} onClick={() => signout(() => Router.replace(`/signin`))}>
